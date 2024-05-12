746c63b33875da4b07bcfe612b76a240884d37d6
Pablo Uraga Martinez <pablouraga01@gmail.com>
2024-05-12 03:25:14 +0200

    Minor route changes

--- hypixelviewer/routes/web.php
+++ hypixelviewer/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\PlayerController;
+use App\Http\Controllers\GeneralController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('index');
 
 Route::get('/player', [PlayerController::class, 'show'])->name('player.show');
+Route::get('/stats/{username}', [GeneralController::class, 'serverStats'])->name('serverStats');
+Route::get('/skyblock/{username}', [GeneralController::class, 'skyblockStats'])->name('skyblockStats');
