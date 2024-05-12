d4f69c66fe48da78c8057a30d6e43a9a18656599
Pablo Uraga Martinez <pablouraga01@gmail.com>
2024-05-12 03:27:02 +0200

    Layout changes

--- hypixelviewer/resources/views/index.blade.php
+++ hypixelviewer/resources/views/index.blade.php
@@ -1,10 +1,16 @@
-<form action="{{ route('player.show') }}" method="get">
-    @csrf
-    <label for="username"></label>
-    <input type="text" name="username" placeholder="Enter player name">
-    <button type="submit">Submit</button>
-</form>
+@extends('homelayout')
 
-@if (session('error'))
-    <p>{{ session('error') }}</p>
-@endif
+@section('title', 'Home - HypixelViewer')
+
+@section('content')
+    <form action="{{ route('player.show') }}" method="get">
+        @csrf
+        <label for="username"></label>
+        <input type="text" name="username" placeholder="Enter player name">
+        <button type="submit">Submit</button>
+    </form>
+
+    @if (session('error'))
+        <p>{{ session('error') }}</p>
+    @endif
+@endsection
