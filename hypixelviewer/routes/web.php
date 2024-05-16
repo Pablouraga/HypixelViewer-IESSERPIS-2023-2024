b8b25dfb293255a79f8dcb565ea1da9818615484
Pablo Uraga Martinez <pablouraga01@gmail.com>
2024-05-16 17:51:46 +0200

    Route changes

--- hypixelviewer/routes/web.php
+++ hypixelviewer/routes/web.php
@@ -4,9 +4,13 @@ use App\Http\Controllers\NavigationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    //Reiniciamos la variable username almacenada en la sesion
+    session(['username' => null]);
     return view('index');
 })->name('index');
 
 Route::get('/player', [NavigationController::class, 'findPlayer'])->name('generalView');
 Route::get('/stats/{username}', [NavigationController::class, 'serverStats'])->name('serverStats');
+Route::get('/auctions/{username}', [NavigationController::class, 'auctionHistory'])->name('auctionHistory');
 Route::get('/skyblock/{username}', [NavigationController::class, 'skyblockStats'])->name('skyblockStats');
+Route::get('/guild/{username}', [NavigationController::class, 'guildDetails'])->name('guildDetails');
