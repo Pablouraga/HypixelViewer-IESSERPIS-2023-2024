4227a8fe6e3d069e79dba5ade74dd04069d1b5fe
Pablo Uraga Martinez <pablouraga01@gmail.com>
2024-05-21 15:33:34 +0200

    Nav changes

--- hypixelviewer/resources/views/partials/homeNav.blade.php
+++ hypixelviewer/resources/views/partials/homeNav.blade.php
@@ -1,5 +1,26 @@
-<ul class="nav">
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('index') }}">HypixelViewer</a>
-    </li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('index') }}">HypixelViewer</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav position-absolute end-0 pe-4">
+            @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('profile') }}">{{ session('username') }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                </li>
+            @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('signup') }}">Sign up</a>
+                </li>
+            @endauth
+        </ul>
+    </div>
+</nav>
