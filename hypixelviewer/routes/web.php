a8e3eff871556badc1772401619c897e901ccdb4
Pablo Uraga Martinez <pablouraga01@gmail.com>
2024-05-21 15:33:16 +0200

    User routes

--- hypixelviewer/routes/web.php
+++ hypixelviewer/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\NavigationController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,3 +15,20 @@ Route::get('/stats/{username}', [NavigationController::class, 'serverStats'])->n
 Route::get('/auctions/{username}', [NavigationController::class, 'auctionHistory'])->name('auctionHistory');
 Route::get('/skyblock/{username}', [NavigationController::class, 'skyblockStats'])->name('skyblockStats');
 Route::get('/guild/{username}', [NavigationController::class, 'guildDetails'])->name('guildDetails');
+
+//Profile routes
+Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+
+Route::get('/login', function () {
+    return view('users.login');
+})->name('loginForm');
+
+Route::post('/login', [UserController::class, 'login'])->name('login');
+
+Route::get('/signup', function () {
+    return view('users.signup');
+})->name('signupForm');
+
+Route::post('/signup', [UserController::class, 'signup'])->name('signup');
+
+Route::get('/logout', [UserController::class, 'logout'])->name('logout');
