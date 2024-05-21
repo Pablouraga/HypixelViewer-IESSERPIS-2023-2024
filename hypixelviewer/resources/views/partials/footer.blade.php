fc6ba3597d42c16cb5448b14408f9a618d7e1057
Pablo Uraga Martinez <pablouraga01@gmail.com>
2024-05-21 15:33:40 +0200

    Added footer

---
+++ hypixelviewer/resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="text-center bg-light">
+    <div class="container p-4">
+
+        <section class="mb-4">
+            <p>
+                Found a bug? Report it <a href="" class="text-dark">here!</a>
+            </p>
+        </section>
+
+    </div>
+
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2024 Copyright:
+        <a class="text-dark" href="https://hypixelviewer.test/">hypixelviewer.test</a>
+    </div>
+
+</footer>
