fb11522fead1dd03a9834230d1e0409fc09c744d
Pablo Uraga Martinez <pablouraga01@gmail.com>
2024-05-27 01:59:06 +0200

    Moved things

---
+++ hypixelviewer/resources/views/player/generalView.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.general')
+
+@section('title', session('username') . ' - HypixelViewer')
+
+@section('content')
+    <h1 class="text-center">{{ session('username') }}</h1>
+    @auth
+        {{-- @dd($favourites) --}}
+        {{-- Icono que al pulsar ejecute la funcion toggleFavourite() dentro de UserController --}}
+        <div>
+            <form action="{{ route('toggleFavourite') }}" method="post">
+                @csrf
+                @method('PATCH')
+                @if ($favourites)
+                    <input type="hidden" name="favourite" value="0">
+                    <button type="submit" class="btn btn-danger">Remove from favourites</button>
+                @else
+                    <input type="hidden" name="favourite" value="1">
+                    <button type="submit" class="btn btn-success">Add to favourites</button>
+                @endif
+            </form>
+        </div>
+    @endauth
+    <img src="https://crafatar.com/avatars/{{ str_replace('-', '', session('uuid')) }}?size=90&overlay" alt="Avatar">
+
+@endsection
