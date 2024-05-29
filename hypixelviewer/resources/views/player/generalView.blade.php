2e6d56a7e19ab0620f20b9790e4e7f5b79788d72
Pablo Uraga Martinez <120651168+Pablouraga@users.noreply.github.com>
2024-05-29 19:25:57 +0200

    If player has user account linked AND its not the account are logged in with, display add button

--- hypixelviewer/resources/views/player/generalView.blade.php
+++ hypixelviewer/resources/views/player/generalView.blade.php
@@ -7,25 +7,38 @@
     @auth
         {{-- Icono que al pulsar ejecute la funcion toggleFavourite() dentro de PlayerController --}}
         <div>
-            
+
             <form action="{{ route('toggleFavourite', $player) }}" method="post">
-                    @csrf
-                    @method('PATCH')
-                    @if ($favourites)
-                        <input type="hidden" name="favourite" value="0">
-                        <button type="submit" class="btn btn-danger">Remove from favourites</button>
-                    @else
-                        <input type="hidden" name="favourite" value="1">
-                        <button type="submit" class="btn btn-success">Add to favourites</button>
-                    @endif
+                @csrf
+                @method('PATCH')
+                @if ($favourites)
+                    <input type="hidden" name="favourite" value="0">
+                    <button type="submit" class="btn btn-danger">Remove from favourites</button>
+                @else
+                    <input type="hidden" name="favourite" value="1">
+                    <button type="submit" class="btn btn-success">Add to favourites</button>
+                @endif
             </form>
         </div>
-        {{-- @dd($player) --}}
+        {{-- Jugador que tiene la cuenta enlazada --}}
+        @isset($linked_account->linked_account)
+            @if ($linked_account->username != Auth::user()->username)
+                <div>
+                    <form action="{{ route('addUser') }}" method="post">
+                        @csrf
+                        @method('POST')
+                        <input type="hidden" name="friend" value="0">
+                        <button type="submit" class="btn btn-warning">Add as friend</button>
+                    </form>
+                    {{-- {{ $linked_account->username }} --}}
+                </div>
+            @endif
+        @endisset
         {{-- Enviar solicitud de amistad / enviar mensaje --}}
-        {{-- <div>
-            
-        </div> --}}
     @endauth
+    @isset($request)
+        @dd($request)
+    @endisset
     <img src="https://crafatar.com/avatars/{{ str_replace('-', '', session('uuid')) }}?size=90&overlay" alt="Avatar">
 
 @endsection
