8ef4899b52d64707fe9e66558c2192f4511d1fbf
Pablo Uraga Martinez <120651168+Pablouraga@users.noreply.github.com>
2024-06-01 20:11:05 +0200

    text model and relation to user

---
+++ hypixelviewer/app/Models/Text.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Text extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'sender',
+        'receiver',
+        'text',
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver');
+    }
+}
