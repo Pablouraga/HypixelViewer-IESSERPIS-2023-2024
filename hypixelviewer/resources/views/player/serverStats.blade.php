4420d78798ca5d081acb10eb5c2124bd7ccde432
Pablo Uraga Martinez <120651168+Pablouraga@users.noreply.github.com>
2024-06-05 13:03:27 +0200

    Display stats from different gamemodes

--- hypixelviewer/resources/views/player/serverStats.blade.php
+++ hypixelviewer/resources/views/player/serverStats.blade.php
@@ -4,8 +4,8 @@
 @section('title', session('username') . ' - HypixelViewer')
 
 @section('content')
-    {{-- @dd($serverStats) --}}
-    <div class="container">
+    <h1 class="text-center mt-3">{{ session('username') }}</h1>
+    <div class="container mb-3 mt-3">
         @if ($serverStats != null)
             <div class="row mt-3">
                 <div class="col-md-4">
@@ -102,28 +102,32 @@
 
                             <div class="coins">
                                 <span class="stat-name">Coins: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Bedwars']['coins'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Bedwars']['coins']) ? $serverStats['stats']['Bedwars']['coins'] : '0' }}</span>
                             </div>
 
                             <div class="played-games">
                                 <span class="stat-name">Played games: </span>
                                 <span
-                                    class="stat-value">{{ $serverStats['stats']['Bedwars']['games_played_bedwars'] }}</span>
+                                    class="stat-value">{{ isset($serverStats['stats']['Bedwars']['games_played_bedwars']) ? $serverStats['stats']['Bedwars']['games_played_bedwars'] : 0 }}</span>
                             </div>
 
                             <div class="won-games">
                                 <span class="stat-name">Won games: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Bedwars']['wins_bedwars'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Bedwars']['wins_bedwars']) ? $serverStats['stats']['Bedwars']['wins_bedwars'] : 0 }}</span>
                             </div>
 
                             <div class="lost-games">
                                 <span class="stat-name">Lost games: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Bedwars']['losses_bedwars'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Bedwars']['losses_bedwars']) ? $serverStats['stats']['Bedwars']['losses_bedwars'] : 0 }}</span>
                             </div>
 
                             <div class="total-kills">
                                 <span class="stat-name">Total kills: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Bedwars']['kills_bedwars'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Bedwars']['kills_bedwars']) ? $serverStats['stats']['Bedwars']['kills_bedwars'] : 0 }}</span>
                             </div>
                         </div>
 
@@ -134,27 +138,32 @@
 
                             <div class="coins">
                                 <span class="stat-name">Coins: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Duels']['coins'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Duels']['coins']) ? $serverStats['stats']['Duels']['coins'] : 0 }}</span>
                             </div>
 
                             <div class="played-games">
                                 <span class="stat-name">Played games: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Duels']['games_played_duels'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Duels']['games_played_duels']) ? $serverStats['stats']['Duels']['games_played_duels'] : 0 }}</span>
                             </div>
 
                             <div class="won-games">
                                 <span class="stat-name">Won games: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Duels']['wins'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Duels']['wins']) ? $serverStats['stats']['Duels']['wins'] : 0 }}</span>
                             </div>
 
                             <div class="lost-games">
                                 <span class="stat-name">Lost games: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Duels']['losses'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Duels']['losses']) ? $serverStats['stats']['Duels']['losses'] : 0 }}</span>
                             </div>
 
                             <div class="total-kills">
                                 <span class="stat-name">Total kills: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Duels']['kills'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Duels']['kills']) ? $serverStats['stats']['Duels']['kills'] : 0 }}</span>
                             </div>
                         </div>
 
@@ -165,32 +174,38 @@
 
                             <div class="coins">
                                 <span class="stat-name">Coins: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Arena']['coins'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Arena']['coins']) ? $serverStats['stats']['Arena']['coins'] : 0 }}</span>
                             </div>
 
                             <div class="win-streaks-1v1">
                                 <span class="stat-name">1v1 win streak: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Arena']['win_streaks_1v1'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Arena']['win_streaks_1v1']) ? $serverStats['stats']['Arena']['win_streaks_1v1'] : 0 }}</span>
                             </div>
 
                             <div class="deaths-1v1">
                                 <span class="stat-name">1v1 deaths: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Arena']['deaths_1v1'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Arena']['deaths_1v1']) ? $serverStats['stats']['Arena']['deaths_1v1'] : 0 }}</span>
                             </div>
 
                             <div class="damage-1v1">
                                 <span class="stat-name">1v1 damage: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Arena']['damage_1v1'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Arena']['damage_1v1']) ? $serverStats['stats']['Arena']['damage_1v1'] : 0 }}</span>
                             </div>
 
                             <div class="played-1v1">
                                 <span class="stat-name">1v1 games played: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Arena']['games_1v1'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Arena']['games_1v1']) ? $serverStats['stats']['Arena']['games_1v1'] : 0 }}</span>
                             </div>
 
                             <div class="lost-1v1">
                                 <span class="stat-name">1v1 games lost: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Arena']['losses_1v1'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Arena']['losses_1v1']) ? $serverStats['stats']['Arena']['losses_1v1'] : 0 }}</span>
                             </div>
                         </div>
 
@@ -201,22 +216,26 @@
 
                             <div class="coins">
                                 <span class="stat-name">Coins: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Quake']['coins'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Quake']['coins']) ? $serverStats['stats']['Quake']['coins'] : 0 }}</span>
                             </div>
 
                             <div class="kills">
                                 <span class="stat-name">Kills: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Quake']['kills'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Quake']['kills']) ? $serverStats['stats']['Quake']['kills'] : 0 }}</span>
                             </div>
 
                             <div class="highest-killstreak">
                                 <span class="stat-name">Highest killstreak: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Quake']['highest_killstreak'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Quake']['highest_killstreak']) ? $serverStats['stats']['Quake']['highest_killstreak'] : 0 }}</span>
                             </div>
 
                             <div class="deaths">
                                 <span class="stat-name">Deaths: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['Quake']['deaths'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['Quake']['deaths']) ? $serverStats['stats']['Quake']['deaths'] : 0 }}</span>
                             </div>
                         </div>
 
@@ -227,27 +246,32 @@
 
                             <div class="coins">
                                 <span class="stat-name">Coins: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['UHC']['coins'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['UHC']['coins']) ? $serverStats['stats']['UHC']['coins'] : 0 }}</span>
                             </div>
 
                             <div class="kills">
                                 <span class="stat-name">Kills: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['UHC']['kills'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['UHC']['kills']) ? $serverStats['stats']['UHC']['kills'] : 0 }}</span>
                             </div>
 
                             <div class="deaths">
                                 <span class="stat-name">Deaths: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['UHC']['deaths'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['UHC']['deaths']) ? $serverStats['stats']['UHC']['deaths'] : 0 }}</span>
                             </div>
 
                             <div class="score">
                                 <span class="stat-name">Score: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['UHC']['score'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['UHC']['score']) ? $serverStats['stats']['UHC']['score'] : 0 }}</span>
                             </div>
 
                             <div class="heads-eaten">
                                 <span class="stat-name">Heads eaten: </span>
-                                <span class="stat-value">{{ $serverStats['stats']['UHC']['heads_eaten'] }}</span>
+                                <span
+                                    class="stat-value">{{ isset($serverStats['stats']['UHC']['heads_eaten']) ? $serverStats['stats']['UHC']['heads_eaten'] : 0 }}</span>
                             </div>
                         </div>
                     </div>
