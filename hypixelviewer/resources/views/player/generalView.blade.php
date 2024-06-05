a713bcacd25f9bbcb6e75a772075c6c42dd0e9e1
Pablo Uraga Martinez <120651168+Pablouraga@users.noreply.github.com>
2024-06-05 23:05:38 +0200

    Minor edit

--- hypixelviewer/resources/views/player/generalView.blade.php
+++ hypixelviewer/resources/views/player/generalView.blade.php
@@ -5,23 +5,8 @@
 @section('content')
     <h1 class="text-center mt-3">{{ session('username') }}</h1>
     <div class="container mb-3 mt-3">
+        <img src="https://crafatar.com/avatars/{{ str_replace('-', '', session('uuid')) }}?size=90&overlay" alt="Avatar">
         @auth
-            {{-- Icono que al pulsar ejecute la funcion toggleFavourite() dentro de PlayerController --}}
-            <div>
-
-                <form action="{{ route('toggleFavourite', $player) }}" method="post">
-                    @csrf
-                    @method('PATCH')
-                    @if ($favourites)
-                        <input type="hidden" name="favourite" value="0">
-                        <button type="submit" class="btn btn-danger">Remove from favourites</button>
-                    @else
-                        <input type="hidden" name="favourite" value="1">
-                        <button type="submit" class="btn btn-success">Add to favourites</button>
-                    @endif
-                </form>
-            </div>
-
             {{-- Jugador que tiene la cuenta enlazada --}}
             @isset($linked_account->linked_account)
                 @if ($linked_account->username != Auth::user()->username)
@@ -44,7 +29,20 @@
                     </div>
                 @endisset
             @endif
+            {{-- Icono que al pulsar ejecute la funcion toggleFavourite() dentro de PlayerController --}}
+            <div>
+                <form action="{{ route('toggleFavourite', $player) }}" method="post">
+                    @csrf
+                    @method('PATCH')
+                    @if ($favourites)
+                        <input type="hidden" name="favourite" value="0">
+                        <button type="submit" class="btn btn-danger">Remove from favourites</button>
+                    @else
+                        <input type="hidden" name="favourite" value="1">
+                        <button type="submit" class="btn btn-success">Add to favourites</button>
+                    @endif
+                </form>
+            </div>
         @endauth
-        <img src="https://crafatar.com/avatars/{{ str_replace('-', '', session('uuid')) }}?size=90&overlay" alt="Avatar">
     </div>
 @endsection
