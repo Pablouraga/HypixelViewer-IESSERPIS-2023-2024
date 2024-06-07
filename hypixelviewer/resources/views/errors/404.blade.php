7c4f5ef7696944ac91e466baadc44c9b94af0535
Pablo Uraga Martinez <120651168+Pablouraga@users.noreply.github.com>
2024-06-07 08:36:01 +0200

    Error view

---
+++ hypixelviewer/resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.general')
+
+@section('title', '404 - Hypixel Viewer')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>404 - Page not found</h1>
+                <p>The page you are looking for does not exist.</p>
+            </div>
+        </div>
+    </div>
+@endsection
