b0fb109a07cf82328b837c36923df8c395371eab
Pablo Uraga Martinez <120651168+Pablouraga@users.noreply.github.com>
2024-06-09 11:52:33 +0200

    Name formatting fix

--- hypixelviewer/resources/views/player/auctionHistory.blade.php
+++ hypixelviewer/resources/views/player/auctionHistory.blade.php
@@ -27,7 +27,7 @@
                         <tbody>
                             @foreach ($auctionsData as $item)
                                 <tr>
-                                    <td>{{ $item['itemName'] }}</td>
+                                    <td>{{ preg_replace('/§[a-zA-Z0-9]/', '', $item['itemName']) }}</td>
                                     <td>
                                         @if ($item['highestBid'] == 0)
                                             Not sold
@@ -66,7 +66,7 @@
                         <tbody>
                             @foreach ($bidsData as $item)
                                 <tr>
-                                    <td>{{ $item['itemName'] }}</td>
+                                    <td>{{ preg_replace('/§[a-zA-Z0-9]/', '', $item['itemName']) }}</td>
                                     <td>
                                         @if ($item['highestOwnBid'] != $item['highestBid'])
                                             Not obtained -
